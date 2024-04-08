0376c48b49a973e1f65fda5e590bcc93b0a2ddca
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-04-08 02:26:31 +0200

    update

--- SandBoxPjtDamier/damier.php
+++ SandBoxPjtDamier/damier.php
@@ -4,7 +4,7 @@ for ($i = 0; $i < 8; $i++) { // ligne
     echo "<tr>";
     for ($j = 0; $j < 8; $j++) { // colonne
         // damier noir et blanc
-        if (($i + $j) % 2 == 0) {
+        if (($i + $j) % 2 == 0) { // si la somme de i et j est paire alors case noire sinon case blanche
             echo "<td id='black'></td>"; // case noire
         } else {
             echo "<td id='white'></td>"; // case blanche
