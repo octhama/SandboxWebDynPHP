179329665c340923000806fae89fb8b425eda93b
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-04-16 12:33:14 +0200

    update

--- SandBoxPjtDamier/index.php
+++ SandBoxPjtDamier/index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Damier PHP</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="assets/js/script.js"></script>
 </head>
 <body>
     <h1>Damier PHP</h1>
