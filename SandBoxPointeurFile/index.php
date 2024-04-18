1fc51c1b9d3e6bea56cbce36110704ea2afd1628
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-04-18 13:08:56 +0200

    update

--- SandBoxPointeurFile/index.php
+++ SandBoxPointeurFile/index.php
@@ -13,21 +13,21 @@
 $documentRoot = $_SERVER['DOCUMENT_ROOT'];
 
 // Ouvrir le dossier document root
-$pointeur = opendir($documentRoot);
+$pointeur = opendir($documentRoot); // Retourne un pointeur de dossier
 
 // Vérifier si l'ouverture du dossier a réussi
 if ($pointeur) {
     // Lire les fichiers et dossiers du document root
-    while (($nomFichier = readdir($pointeur)) !== false) {
+    while (($nomFichier = readdir($pointeur)) !== false) { // Lire le nom du fichier ou du dossier
         // Afficher le nom du fichier ou du dossier
-        echo $nomFichier . "<br>";
+        echo $nomFichier . "<br>"; // Affiche le nom du fichier ou du dossier
     }
 
     // Fermer le pointeur
     closedir($pointeur);
 } else {
     // Erreur d'ouverture du dossier
-    echo "Erreur d'ouverture du document root";
+    echo "Erreur d'ouverture du document root"; // Affiche un message d'erreur
 }
 
 ?>
