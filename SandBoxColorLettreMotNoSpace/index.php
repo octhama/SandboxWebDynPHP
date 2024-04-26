e63afbcc47d12bf13d756d54073a72a316e13d7a
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-04-26 19:03:01 +0200

    update

--- SandBoxColorLettreMotNoSpace/index.php
+++ SandBoxColorLettreMotNoSpace/index.php
@@ -8,7 +8,6 @@ foreach ($phrase as $lettre) {
     if ($lettre == " ") {
         echo $lettre;
     } else {
-        // Différente couleur pour chaque caractère de mots d'une phrase en entrée en couleur sauf les espaces
         echo "\033[" . rand(31, 37) . "m" . $lettre . "\033[0m"; // Couleur aléatoire pour chaque lettre
     }
 }
