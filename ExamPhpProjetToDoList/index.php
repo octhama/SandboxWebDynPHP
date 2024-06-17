6bdf34a38855b74194968ab3d24e76b28b881ca8
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-06-17 19:18:11 +0200

    update

--- ExamPhpProjetToDoList/index.php
+++ ExamPhpProjetToDoList/index.php
@@ -11,7 +11,8 @@ $selectedProgress = isset($_POST['newProgress']) ? intval($_POST['newProgress'])
 <head>
     <meta charset="UTF-8">
     <title>Liste de tâches</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
@@ -21,19 +22,21 @@ $selectedProgress = isset($_POST['newProgress']) ? intval($_POST['newProgress'])
 
     <form action="add.php" method="post" class="mb-3">
         <div class="input-group">
-            <input type="text" id="newTask" name="newTask" class="form-control me-2" placeholder="Nouvelle tâche" required>
+            <input type="text" id="newTask" name="newTask" class="form-control me-2" placeholder="Nouvelle tâche"
+                   required>
             <input type="date" id="newDate" name="newDate" class="form-control me-2">
             <input type="time" id="newTime" name="newTime" class="form-control me-2">
             <select name="newPriority" class="form-select me-2">
                 <?php echo getPriorityOptions(1); ?>
             </select>
             <select name="newProgress" class="form-select me-2">
-                <?php echo getProgressOptions(0);?>
+                <?php echo getProgressOptions(0); ?>
             </select>
             <select name="newCategory[]" class="form-select me-2">
                 <?php echo getCategoryOptions(''); ?>
             </select>
-            <input type="text" id="newCustomCategory" name="newCustomCategory" class="form-control me-2" placeholder="Caté. perso.">
+            <input type="text" id="newCustomCategory" name="newCustomCategory" class="form-control me-2"
+                   placeholder="Caté. perso.">
             <button type="submit" class="btn btn-primary">Ajouter</button>
         </div>
     </form>
@@ -54,8 +57,18 @@ $selectedProgress = isset($_POST['newProgress']) ? intval($_POST['newProgress'])
             </thead>
             <tbody>
             <?php foreach ($todos as $todo): ?>
-                <tr class="<?php echo $todo['completed'] ? 'text-decoration-line-through' : ''; ?>"> <!-- Ajout de la classe CSS pour barrer le texte si la tâche est terminée -->
-                    <td><?php echo htmlspecialchars($todo['task'] ?? ''); ?></td>
+
+                <tr>
+                    <!-- Ajout de la classe CSS Bootstrap pour barrer le texte en rouge si la tâche est terminée -->
+                    <td>
+                        <?php if ($todo['completed']): ?>
+                            <span class="text-decoration-line-through text-danger">
+                                <?php echo htmlspecialchars($todo['task'] ?? ''); ?>
+                            </span>
+                        <?php else: ?>
+                            <?php echo htmlspecialchars($todo['task'] ?? ''); ?>
+                        <?php endif; ?>
+                    </td>
                     <td><?php echo htmlspecialchars($todo['date'] ?? ''); ?></td>
                     <td><?php echo htmlspecialchars($todo['time'] ?? ''); ?></td>
                     <td>
@@ -83,20 +96,26 @@ $selectedProgress = isset($_POST['newProgress']) ? intval($_POST['newProgress'])
                                 $progressClass = 'bg-success';
                             }
                             ?>
-                            <div class="progress-bar <?php echo $progressClass; ?> progress-bar-striped" role="progressbar" style="width: <?php echo $progress; ?>%" aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar <?php echo $progressClass; ?> progress-bar-striped"
+                                 role="progressbar" style="width: <?php echo $progress; ?>%"
+                                 aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0" aria-valuemax="100">
                                 <?php echo $progress; ?>%
                             </div>
                         </div>
                     </td>
                     <td>
                         <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheck<?php echo $todo['id']; ?>" <?php echo $todo['completed'] ? 'checked' : ''; ?> onclick="location.href='toggle.php?id=<?php echo urlencode($todo['id']); ?>'">
-                            <label class="form-check-label" for="flexSwitchCheck<?php echo $todo['id']; ?>"><?php echo $todo['completed'] ? 'Oui' : 'Non'; ?></label>
+                            <input class="form-check-input" type="checkbox" role="switch"
+                                   id="flexSwitchCheck<?php echo $todo['id']; ?>" <?php echo $todo['completed'] ? 'checked' : ''; ?>
+                                   onclick="location.href='toggle.php?id=<?php echo urlencode($todo['id']); ?>'">
+                            <label class="form-check-label"
+                                   for="flexSwitchCheck<?php echo $todo['id']; ?>"><?php echo $todo['completed'] ? 'Oui' : 'Non'; ?></label>
                         </div>
                     </td>
                     <td>
                         <a href="edit.php?id=<?php echo urlencode($todo['id']); ?>" class="btn btn-warning">Modifier</a>
-                        <a href="delete.php?id=<?php echo urlencode($todo['id']); ?>" class="btn btn-danger">Supprimer</a>
+                        <a href="delete.php?id=<?php echo urlencode($todo['id']); ?>"
+                           class="btn btn-danger">Supprimer</a>
                     </td>
                 </tr>
             <?php endforeach; ?>
@@ -107,7 +126,9 @@ $selectedProgress = isset($_POST['newProgress']) ? intval($_POST['newProgress'])
     <?php endif; ?>
 </div>
 <?php include 'views/layout/footer.php'; ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
 </body>
 </html>
