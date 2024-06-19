0130f46a122ff4bb456caefc58431e12f711db7b
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-06-19 23:47:20 +0200

    update

--- ExamPhpProjetToDoList/views/layout/navbar.php
+++ ExamPhpProjetToDoList/views/layout/navbar.php
@@ -1,31 +1,35 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="index.php">ToDo List</a>
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/brand/Backlog.svg" alt="Bootstrap" width="60" height="48">ToDoList
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div id="navbarNav" class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="trash.php"><i class="bi bi-trash"></i></a>
-                    </li>
+
                 </ul>
                 <div class="dropdown" data-bs-theme="dark">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonDark" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="bi bi-moon"></i>
                     </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonDark">
-                        <li class="nav-item">
-                            <a class="nav-link" href="?mode=jour">
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButtonDark">
+                        <li>
+                            <a class="dropdown-item" href="?mode=jour">
                                 <i class="bi bi-sun"></i> Mode Jour
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="?mode=nuit">
+                        <li>
+                            <a class="dropdown-item" href="?mode=nuit">
                                 <i class="bi bi-moon"></i> Mode Nuit
                             </a>
+                        </li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <a class="dropdown-item" href="trash.php"><i class="bi bi-trash"></i> Corbeille</a>
                         </li>
                     </ul>
                 </div>
