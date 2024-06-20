b73c6113d05435abf96d12884fe833e76cf2547d
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-06-20 23:03:41 +0200

    update

--- ExamPhpProjetToDoList/views/layout/footer.php
+++ ExamPhpProjetToDoList/views/layout/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer mt-auto py-3">
     <div class="container text-center">
-        <span>&copy; <?php echo date('Y'); ?> ToDo List</span>
+        <span>&copy; <?php echo date('Y'); ?> ToDoList</span>
     </div>
 </footer>
