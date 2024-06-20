97aaf3e3c1da167eb555aadf28905d2535cd71cf
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-06-20 22:38:47 +0200

    update

--- ExamPhpProjetToDoList/views/layout/navbar.php
+++ ExamPhpProjetToDoList/views/layout/navbar.php
@@ -2,9 +2,8 @@
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
             <a class="navbar-brand" href="index.php">
-                <img src="assets/brand/Backlog.svg" alt="Bootstrap" width="60" height="48" class="d-inline-block align-text-top">
-                ToDoList
-            </a>
+                <img src="assets/brand/Backlog.svg" alt="Bootstrap" width="30" height="24" class="d-inline-block align-text-top">
+                ToDoList </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
