98d029bdb0c27047641ae6bb51b861040fb5c1c1
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-06-20 23:08:54 +0200

    update

--- ExamPhpProjetToDoList/add.php
+++ ExamPhpProjetToDoList/add.php
@@ -9,11 +9,18 @@ if (isset($_POST['newTask'])) {
     $newProgress = intval($_POST['newProgress'] ?? 0); // intval() pour s'assurer que la valeur est un entier (0 par défaut)
     $newCategories = $_POST['newCategory'] ?? [];
 
+    // Ajouter la nouvelle catégorie personnalisée si elle n'est pas vide et n'existe pas déjà dans les catégories prédéfinies
     if (!empty($_POST['newCustomCategory'])) {
         $newCategories[] = $_POST['newCustomCategory'];
     }
 
     $newCategoryString = implode(',', $newCategories); // Convertir le tableau en chaîne séparée par des virgules (si plusieurs catégories)
+    //PS : implode() est un fonction PHP qui permet de convertir un tableau en chaîne de caractères en les séparant par un délimiteur
+    // Exemple : implode(',', ['Travail', 'Personnel', 'Autre']) retourne 'Travail, Personnel, Autre')
+    // On a aussi la fonction join() qui fait la même chose que implode() mais avec les arguments inversés : join(',', ['Travail', 'Personnel', 'Autre'])
+    // explode() est l'inverse de implode() : il permet de convertir une chaîne de caractères en tableau en utilisant un délimiteur
+    // Exemple : explode(',', 'Travail,Personnel,Autre') retourne ['Travail', 'Personnel', 'Autre'])
+    
 
     $todos = getTodos();
     // Création d'un nouveau tableau associatif pour la nouvelle tâche à ajouter
