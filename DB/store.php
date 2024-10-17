af60cf13a2f56f287c7771209c3dd4c948ce7752
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-10-17 19:49:43 +0200

    update

--- DB/store.php
+++ DB/store.php
@@ -3,8 +3,10 @@ echo '<pre>';
 var_dump($_POST);
 echo '</pre>';
 
-// si un des trois champs est vide on retourne dans le ffoormulaire sinon on retourne à la page index.php
+// Si un des trois champs est vide on retourne dans le ffoormulaire sinon on retourne à la page index.php
 if (empty($_POST['name']) || empty($_POST['prenom']) || empty($_POST['email'])) {
+    $db = new db(); // on instancie la classe db pour pouvoir utiliser la méthode store qui permet d'enregistrer les données dans la base de données
+    $db->store($_POST); // on enregistre les données dans la base de données
     header('Location: create.php');
     exit();
 }
