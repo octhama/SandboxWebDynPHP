de182c18385dcf267a2122f3592e6fcfbd66e23f
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-10-17 19:37:57 +0200

    update

--- DB/store.php
+++ DB/store.php
@@ -2,3 +2,10 @@
 echo '<pre>';
 var_dump($_POST);
 echo '</pre>';
+
+// si un des trois champs est vide on retourne dans le ffoormulaire sinon on retourne à la page index.php
+if (empty($_POST['name']) || empty($_POST['prenom']) || empty($_POST['email'])) {
+    header('Location: create.php');
+    exit();
+}
+exit();
