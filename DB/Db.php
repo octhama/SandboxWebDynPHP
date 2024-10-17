a6035b4ebd0cd643c06d760f950e7b8852db22f2
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-10-17 19:56:57 +0200

    update

--- DB/Db.php
+++ DB/Db.php
@@ -1,1 +1,16 @@
 <?php
+// Créer une classe Db qui permet de se connecter à la base de données et d'enregistrer les données dans la base de données avec un attribut connexion
+class Db
+{
+    private $conn;
+    public function __construct()
+    {
+        $this->conn = new PDO('sqlite:esadb.db'); // on se connecte à la base de données
+    }
+    public function store($data) // on crée une méthode store qui permet d'enregistrer les données dans la base de données
+    {
+        $sql = 'INSERT INTO users (nom, prenom, email) VALUES (:nom, :prenom, :email)'; // on crée une requête sql pour insérer les données dans la base de données
+        $stmt = $this->conn->prepare($sql); // on prépare la requête sql
+        $stmt->execute($data); // on exécute la requête sql
+    }
+}
