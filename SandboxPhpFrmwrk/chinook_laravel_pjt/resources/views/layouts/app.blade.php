ee7c61f426b9eeb96fba62d2885ca796e1d29eb0
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-12-11 00:42:18 +0100

    update

--- SandboxPhpFrmwrk/chinook_laravel_pjt/resources/views/layouts/app.blade.php
+++ SandboxPhpFrmwrk/chinook_laravel_pjt/resources/views/layouts/app.blade.php
@@ -8,9 +8,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">Chinook Laravel App Sandbox</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('customers.index') }}">Clients</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('employees.index') }}">Employés</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main>
     @yield('content')
-</div>
+</main>
+
+<footer class="bg-dark text-white text-center py-3">
+    <p>&copy; {{ date('Y') }} Chinook Laravel App Sandbox. Tous droits réservés.</p>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- SandboxPhpFrmwrk/chinook_laravel_pjt/routes/web.php
+++ SandboxPhpFrmwrk/chinook_laravel_pjt/routes/web.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\EmployeeController;
 
 Route::resource('customers', CustomerController::class); // Ressource route pour les clients : index, create, store, show, edit, update, destroy
+Route::resource('employees', EmployeeController::class); // Ressource route pour les employés : index, create, store, show, edit, update, destroy
+Route::get('/', function () {
+    return view('welcome');
+});
