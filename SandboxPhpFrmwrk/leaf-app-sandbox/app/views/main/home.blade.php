01e05d1117f97e4bfcb06d112695db866a4e1d93
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-12-11 17:08:01 +0100

    update

--- SandboxPhpFrmwrk/leaf-app-sandbox/app/views/main/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('layouts.app')
-
-@section('content')
-    <h1>Hello poto</h1>
-    <div class="mt-16 mb-32 rounded-xl border border-[rgba(172,175,176,0.3)] py-3 px-8 [font-family:Berkeley_Mono]">
-    <input type="text" class="border border-slate-300 hover:border-slate-500 py-3 px-6 rounded-full" placeholder="Search">
-    </div>
-    <a href="/about">Retour vers la page about</a>
-@endsection
