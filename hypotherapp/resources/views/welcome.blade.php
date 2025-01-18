703c89d6fd599ca5523f5b7ea68b87ccf146a511
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-01-18 08:56:54 +0100

    update

--- hypotherapp/resources/views/welcome.blade.php
+++ hypotherapp/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Page</title>
+    <title>Hypotherapp - Welcome Page</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome for Icons -->
@@ -26,7 +26,6 @@
         h2 {
             color: #343a40;
             font-weight: bold;
-            text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.2);
         }
 
         /* Menu Cards Styling */
@@ -77,6 +76,9 @@
     </style>
 </head>
 <body>
+<div class="container my-5">
+    <h1 class="text-center mb-4">Bienvenue sur Hypotherapp</h1>
+</div>
 <!-- Main Menu Section -->
 <div class="container my-5">
     <h2 class="text-center mb-4">Navigation principale</h2>
